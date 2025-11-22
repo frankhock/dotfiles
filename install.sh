24f45a58ec352c59dda3596e69bf99e1238cb64c
Frank Hock <frank.hock@gmail.com>
2025-11-22 07:28:08 -0500

    and zed symlinks to install script

--- install.sh
+++ install.sh
@@ -5,4 +5,7 @@ ln -sf ~/dotfiles/shell/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/shell/.aliases ~/.aliases
 
 ln -sf ~/dotfiles/shell/starship.toml ~/.config/starship.toml
+
+ln -sf ~/dotfiles/zed/keymap.json ~/.config/zed/keymap.json
 ln -sf ~/dotfiles/zed/settings.json ~/.config/zed/settings.json
+ln -sf ~/dotfiles/zed/tasks.json ~/.config/zed/tasks.json
