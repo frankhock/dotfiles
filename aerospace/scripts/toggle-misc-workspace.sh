c4b0de19b95f4e0e1dee4b149fb3aa7e43f59b71
Frank Hock <frank.hock@gmail.com>
2026-09-22 10:06:21 -0400

    Remove aerospace and autobots directories

--- aerospace/scripts/toggle-misc-workspace.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-W=$(aerospace list-workspaces --focused)
-if [ "$W" = "6" ]; then
-  aerospace move-node-to-workspace 7
-  aerospace workspace 7
-elif [ "$W" = "7" ]; then
-  aerospace move-node-to-workspace 6
-  aerospace workspace 6
-fi
